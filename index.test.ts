18fe0b696bdc119b5d1265fdd61f84ac765ce838
Lee Tae Woo <syr@meu.works>
2019-09-05 10:52:03 +0900

    Change test code

--- index.test.ts
+++ index.test.ts
@@ -4,35 +4,41 @@ import {
     GRPCClient,
 } from './src';
 
-interface IReaderMethods extends IGRPCClientMapOfMethods {
-    // getReader: (argument: { type: 'id' | 'username' | 'email' }) => Promise<Reader>;
-    getReader: GRPCClientCallMethod<
+interface IUserMethods extends IGRPCClientMapOfMethods {
+    getUser: GRPCClientCallMethod<
+        // RequestType
         { type: 'id' | 'username' | 'email'; value: string },
+        // ResponseType
         { id: string; username: string; email: string; created_at: string }
     >;
-    addReader: GRPCClientCallMethod<
+    addUser: GRPCClientCallMethod<
+        // RequestType
         { username: string; email: string },
+        // ResponseType
         { id: string; username: string; email: string; created_at: string }
     >;
 }
 
 (async () => {
-    const client = new GRPCClient<IReaderMethods>({
-        filepath: './proto/reader.proto',
-        address: '0.0.0.0:11111',
-        package: 'Reader',
-        service: 'ReaderService',
+    const client = new GRPCClient<IUserMethods>({
+        filepath: './user.proto',
+        address: '0.0.0.0:12345',
+        package: 'User',
+        service: 'UserService',
     });
 
-    await client.call('addReader', { username: 'syr', email: 'syr@meu.works' });
+    await client.call('addUser', {
+        username: 'syr',
+        email: 'syr@meu.works',
+    });
 
-    const reader = await client.call('getReader', {
+    const user = await client.call('getUser', {
         type: 'email',
         value: 'syr@meu.works',
     });
 
-    reader.id;
-    reader.username;
-    reader.email;
-    reader.created_at;
+    user.id;
+    user.username;
+    user.email;
+    user.created_at;
 })();
